6e272299fcc2a9c326fa0e42fa53136931bb1f0a
chaotic-justice <deepocean.2900@gmail.com>
2025-05-18 17:42:42 -0400

    comment out dirname.

--- src/collections/Media.ts
+++ src/collections/Media.ts
@@ -11,7 +11,7 @@ import { fileURLToPath } from 'url'
 import { anyone } from '../access/anyone'
 import { authenticated } from '../access/authenticated'
 
-const filename = fileURLToPath(import.meta.url)
+// const filename = fileURLToPath(import.meta.url)
 // const dirname = path.dirname(filename)
 
 export const Media: CollectionConfig = {
