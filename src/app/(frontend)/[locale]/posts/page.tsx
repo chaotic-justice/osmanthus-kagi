8785d77fa5eae2aede4019dcf9150e631658250b
chaotic-justice <deepocean.2900@gmail.com>
2025-05-22 11:32:44 -0400

    cancel force-dynamic

--- src/app/(frontend)/[locale]/posts/page.tsx
+++ src/app/(frontend)/[locale]/posts/page.tsx
@@ -7,7 +7,7 @@ import configPromise from '@payload-config'
 import { getPayload, type TypedLocale } from 'payload'
 import PageClient from './page.client'
 
-export const dynamic = 'force-dynamic'
+// export const dynamic = 'force-dynamic'
 // export const revalidate = 600
 
 type Args = {
